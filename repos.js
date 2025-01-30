e60ab36a8df20a1344e60d56f435c4d2acc28296
Rooster <113987912+Roschool@users.noreply.github.com>
2025-01-30 21:41:30 +0100

    Update and rename script.js to repos.js

---
+++ repos.js
@@ -0,0 +1,5 @@
+const repos = [
+    { name: "School project website", url: "https://roschool.github.io/website/" },
+    { name: "Project 2", url: "https://roschool.github.io/project2/" },
+    { name: "Project 3", url: "https://roschool.github.io/project3/" }
+];
