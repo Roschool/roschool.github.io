5a56b9dbea62b08bf968a4e54f2ddfcd5a9aeac6
Rooster <113987912+Roschool@users.noreply.github.com>
2025-01-30 21:50:21 +0100

    Update repos.js

--- repos.js
+++ repos.js
@@ -1,5 +1,17 @@
 const repos = [
-    { name: "School project website", url: "https://roschool.github.io/website/" },
-    { name: "Project 2", url: "https://roschool.github.io/project2/" },
-    { name: "Project 3", url: "https://roschool.github.io/project3/" }
+    {
+        name: "Module website school",
+        url: "https://roschool.github.io/website/",
+        description: "In de module moest je een website maken en dit is de website."
+    },
+    {
+        name: "Nepnieuws website",
+        url: "https://europenews.github.io/",
+        description: "Een nepnieuws website die we voor een schoolproject moesten maken."
+    },
+    {
+        name: "Code Executor",
+        url: "https://roschool.github.io/code-executor/",
+        description: "Een nuttige tool voor code gelijk uit te voeren (HTML)."
+    }
 ];
